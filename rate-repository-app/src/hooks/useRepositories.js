24a56202382702ff835ad6427dbed7aadf106d13
JA8866 <109983530+JA8866@users.noreply.github.com>
2026-07-14 01:06:26 -0500

    finished chapter 4 exercise 11

---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+import { useQuery } from '@apollo/client/react';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  return { repositories: data?.repositories, loading };
+};
+
+export default useRepositories;
